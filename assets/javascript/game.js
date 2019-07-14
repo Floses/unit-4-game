27d11858f5db6788007125f128ac298df8d40025
Spicy Sampler <50890113+Floses@users.noreply.github.com>
2019-07-14 12:49:57 -0700

    made variables for random numbers for our 4 gems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,10 @@
 $(document).ready(function() {
   var randomNum = Math.floor(Math.random() * 102) + 19;
   $(".randomNum").text(randomNum);
+
+  // setting random numbers for each of our gems and storing them in variables
+  var gem1 = Math.floor(Math.random() * 12) + 1;
+  var gem2 = Math.floor(Math.random() * 12) + 1;
+  var gem3 = Math.floor(Math.random() * 12) + 1;
+  var gem4 = Math.floor(Math.random() * 12) + 1;
 });
