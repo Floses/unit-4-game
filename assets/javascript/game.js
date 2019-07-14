2b412444bd31ba66cb867cc11f202c7589fe9b38
Spicy Sampler <50890113+Floses@users.noreply.github.com>
2019-07-14 13:07:46 -0700

    added on click events for each of our gems and had them display their values to our totalscore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,12 +8,30 @@ $(document).ready(function() {
   var gem2 = Math.floor(Math.random() * 12) + 1;
   var gem3 = Math.floor(Math.random() * 12) + 1;
   var gem4 = Math.floor(Math.random() * 12) + 1;
-
+  // setting our variables for wins losses and totalScore
   var wins = 0;
   var losses = 0;
   var totalScore = 0;
-
+  // appending our values to the html
   $("#wins").append(wins);
   $("#losses").append(losses);
   $(".totalScore").append(totalScore);
+  // here we will create our on click elements for our gems
+
+  $("#blue").on("click", function() {
+    totalScore = totalScore + gem1;
+    $(".totalScore").text(totalScore);
+  });
+  $("#orange").on("click", function() {
+    totalScore = totalScore + gem2;
+    $(".totalScore").text(totalScore);
+  });
+  $("#purple").on("click", function() {
+    totalScore = totalScore + gem3;
+    $(".totalScore").text(totalScore);
+  });
+  $("#red").on("click", function() {
+    totalScore = totalScore + gem4;
+    $(".totalScore").text(totalScore);
+  });
 });
