077146bfdbc9f124b9732f91238383a4721165c5
Spicy Sampler <50890113+Floses@users.noreply.github.com>
2019-07-14 11:23:48 -0700

    inserted our jquery and started our code for our game

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// on page start I need to generate a random number for people to guess and insert it into our random number div
+$(document).ready(function() {});
+// also on page start I need to generate for seperate random numbers and assign them one to each gem for the player guesses
+
+// need to make on click events for each gem to grab whatever number that the gem holds and add it to their guessed score
+
+// I need to show every number for each gem clicked added to the players score
+
+// need to add win and loss conditions
+
+// if the player wins I need it to display "you win!"
+
+// if the player wins I need to add the win to their win counter
+
+// if the player loses I need it to display "you lose!"
+
+// if the player loses I need to add the loss to their loss counter
+
+// regardless of whether the player wins or loses, I need the game to restart upon completion, generating new numbers
+// for the gems and new numbers to guess
