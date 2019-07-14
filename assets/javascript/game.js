d89c5cdcbed704e1d7d958887d19df3a77319603
Spicy Sampler <50890113+Floses@users.noreply.github.com>
2019-07-14 13:37:07 -0700

    add a game reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,17 +21,64 @@ $(document).ready(function() {
   $("#blue").on("click", function() {
     totalScore = totalScore + gem1;
     $(".totalScore").text(totalScore);
+    if (totalScore == randomNum) {
+      win();
+    } else if (totalScore > randomNum) {
+      lose();
+    }
   });
   $("#orange").on("click", function() {
     totalScore = totalScore + gem2;
     $(".totalScore").text(totalScore);
+    if (totalScore == randomNum) {
+      win();
+    } else if (totalScore > randomNum) {
+      lose();
+    }
   });
   $("#purple").on("click", function() {
     totalScore = totalScore + gem3;
     $(".totalScore").text(totalScore);
+    if (totalScore == randomNum) {
+      win();
+    } else if (totalScore > randomNum) {
+      lose();
+    }
   });
   $("#red").on("click", function() {
     totalScore = totalScore + gem4;
     $(".totalScore").text(totalScore);
+    if (totalScore == randomNum) {
+      win();
+    } else if (totalScore > randomNum) {
+      lose();
+    }
   });
+
+  // here we will add the functions for winning and losing
+
+  function win() {
+    $(".outcome").text("YOU WIN!");
+    wins++;
+    gameReset();
+  }
+  function lose() {
+    $(".outcome").text("YOU LOSE!");
+    losses++;
+    gameReset();
+  }
+
+  // this will be the function to reset the game
+
+  function gameReset() {
+    var randomNum = Math.floor(Math.random() * 102) + 19;
+    $(".randomNum").text(randomNum);
+
+    gem1 = Math.floor(Math.random() * 12) + 1;
+    gem2 = Math.floor(Math.random() * 12) + 1;
+    gem3 = Math.floor(Math.random() * 12) + 1;
+    gem4 = Math.floor(Math.random() * 12) + 1;
+    totalScore = 0;
+    $(".totalScore").text(totalScore);
+  }
 });
